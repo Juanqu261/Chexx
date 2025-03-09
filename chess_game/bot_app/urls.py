5cbd79300cb944e12d2fa764a9e75cf6705e4eaa
Juanqu <jfquintero261@gmail.com>
2025-03-09 10:52:41 -0500

    Fixed board display

--- chess_game/bot_app/urls.py
+++ chess_game/bot_app/urls.py
@@ -1,8 +1,10 @@
 # bot_app/urls.py
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.play_chess, name='play_chess'),
     path('bot_move/', views.bot_move, name='bot_move'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
